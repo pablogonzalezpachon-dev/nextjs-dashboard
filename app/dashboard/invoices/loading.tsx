14b159803bab6fab4b8d80e86025073e5d7c02df
Pablo <pablo.gonzalezpachon@gmail.com>
2025-07-25 23:27:18 -0500

    Performance

--- app/dashboard/invoices/loading.tsx
+++ app/dashboard/invoices/loading.tsx
@@ -10,14 +10,9 @@ export default function Loading() {
       <div className="flex w-full items-center justify-between">
         <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
       </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search invoices..." />
-        <CreateInvoice />
-      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8"></div>
       <InvoicesTableSkeleton />
-      <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={7} />
-      </div>
+      <div className="mt-5 flex w-full justify-center"></div>
     </div>
   );
 }
