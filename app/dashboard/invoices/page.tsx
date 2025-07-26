70107bc9ced32b79837bea7403bde8b744726c88
Pablo <pablo.gonzalezpachon@gmail.com>
2025-07-25 23:08:01 -0500

    Improving performance

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -36,7 +36,9 @@ export default async function Page(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        <Pagination totalPages={totalPages} />
+        <Suspense fallback={<Pagination totalPages={7} />}>
+          <Pagination totalPages={totalPages} />
+        </Suspense>
       </div>
     </div>
   );
